10eff29dd8c4d8620b25f777447567daf350cf9a
Vasilisa <grigvasilisa@gmail.com>
2019-12-19 22:13:05 +0300

    with rools

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from Board_class import Board
 
 pygame.init()
-size = (width, height) = 1000, 800
+size = (width, height) = 800, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
@@ -33,21 +33,32 @@ def terminate():
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+    intro_text = ["Ера динозавров", "",
+                  "Вы попали в джунгли Юрского периода,",
+                  "и Вам предстоит добраться до своего",
+                  "реактивного самолёта, минуя динозавров",
+                  "и избегая ударов метеоритов.",
+                  'Будьте осторожны и внимательны:',
+                  'дорога таит в себе много опасностей! ']
 
     fon = pygame.transform.scale(load_image('start_dino.jpg'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
+    line = intro_text[0]
+    string_rendered = font.render(line, 6, pygame.Color('black'))
+    intro_rect = string_rendered.get_rect()
     text_coord = 50
-    for line in intro_text:
+    intro_rect.top = text_coord
+    intro_rect.x = 310
+    text_coord += intro_rect.height
+    screen.blit(string_rendered, intro_rect)
+    text_coord = 60
+    for line in intro_text[1:]:
         string_rendered = font.render(line, 1, pygame.Color('black'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        intro_rect.x = 200
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
 
